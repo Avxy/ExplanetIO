40c62ba7815cc163f054e490efa8446161bc3c10
Avxy <Avxy>
2021-05-15 12:53:48 -0300

    publish website

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/peerjs', peer);
 // })
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname,'./public_html/blockland/spa/public/index.html'))
+	res.sendFile('./public_html/blockland/spa/public/index.html')
   })
 
   app.get('/hexlibrium', (req, res) => {
